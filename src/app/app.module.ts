552f3711d486ccb5582c511dac0c806152b13699
Nauman Tanwir <nauman.tanwir@IN-IT1446.local>
2019-01-15 02:02:08 +0530

    cars list, cars service and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './nav/nav.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarsDetailsComponent } from './cars-details/cars-details.component';
+import { AddCarsComponent } from './add-cars/add-cars.component';
+import { EditCarsComponent } from './edit-cars/edit-cars.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CarsComponent,
+    CarsDetailsComponent,
+    AddCarsComponent,
+    EditCarsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
